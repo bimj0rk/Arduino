b846022d34ece222c1c66bfb1330b1ae92d865a0
Adrian <aicampean@gmail.com>
2023-06-16 15:48:56 +0300

    pchardwaremonitoring tool and MA project update

--- UPB/Year II/Semester 2/Microprocessor Architecture/Project/Project.ino
+++ UPB/Year II/Semester 2/Microprocessor Architecture/Project/Project.ino
@@ -1,120 +1,109 @@
 #include <Servo.h>
-#include "SR04.h"
-#include "IRremote.h"
+#include <SR04.h>
+#include <IRremote.h>
 #define trigPin 12
 #define echoPin 11
 
-int activeBuzzer = 13; //buzzer-ul este conectat pe pinul 13
+int activeBuzzer = 13;
 
-Servo servoMotor; //initializez servo-ul
-int position = 0; //pozitia initiala: 0
+Servo servoMotor;
+int position = 0;
+bool open = false;
 
-
-//initializez senzorul ultrasonic
 SR04 ultrasonicSensor = SR04(echoPin, trigPin);
 long distance;
 
-
-//initializez senzorul IR
 int reciever = 9;
 IRrecv irrecv(reciever);
 decode_results results;
-int remoteButton;
+bool autonomousLock = false;
+bool enableAuto = false;
 
 void setup(){
   Serial.begin(9600); 
-  pinMode(activeBuzzer, OUTPUT); //activez buzzer-ul ca output */
-  servoMotor.attach(10); //servo-ul este conectat pe pinul 10
-  irrecv.enableIRIn(); //activez reciever-ul IR
+  pinMode(activeBuzzer, OUTPUT);
+  servoMotor.attach(10);
+  irrecv.enableIRIn();
 }
 
-//traducerea butoanelor folosite de pe telecomanda IR
 void translateIR(){
   switch(results.value){
-    case 0xFF629D: //butonul CH de pe telecomanda, va fi folosit pentru modul autonom
-      autonomousMode();
+    case 0xFF629D:
+      if(!autonomousLock){
+        enableAuto = false;
+        autonomousLock = true;
+        Serial.println(enableAuto);
+      } else {
+        enableAuto = true;
+        autonomousLock = false;
+        Serial.println(enableAuto);
+      }
       break;
       
-    case 0xFFA857: //butonul + de pe telecomanda, va fi folosit pentru deschiderea manuala
+    case 0xFFA857:
       manualOverrideOpen();
       break;
 
-    case 0xFFE01F: //butonul - de pe telecomanda, va fi folosit pentru inchiderea manuala
+    case 0xFFE01F:
       manualOverrideClose();
       break;
 
     default:
-      Serial.println("Button not found");
+      break;
   }
 }
 
 void autonomousMode(){
-  Serial.println("Autonomous mode");
-  //servo-ul va deschide usor capacul
   for(position = 0; position <= 115; position++){ 
       servoMotor.write(position);            
       delay(15);
   }
 
-  delay(8000); //timp de asteptate: 8 secunde
+  delay(8000);
 
-  //sunet de avertizare
   digitalWrite(activeBuzzer, HIGH);
   delay(1);
   digitalWrite(activeBuzzer, LOW);
 
-   //timp de asteptare: 2 secunde
   delay(2000);
 
-  //servo-ul inchide capacul
   servoMotor.write(20);
-
-  Serial.println("Autonomous mode ended");
 }
 
 void manualOverrideOpen(){
-  Serial.println("Manual open");
-
-  //sunet de avertizare
   digitalWrite(activeBuzzer, HIGH);
   delay(1);
   digitalWrite(activeBuzzer, LOW);
   delay(1000);
 
-  //servo-ul va deschide usor capacul
   for(int position = 0; position <= 115; position++){
       servoMotor.write(position);            
       delay(15);
   }
 
-  //sunet de avertizare
   digitalWrite(activeBuzzer, HIGH);
   delay(1);
   digitalWrite(activeBuzzer, LOW);
   delay(1);
-
-  Serial.println("Manual open ended");
+  
+  open = true;
 }
 
 void manualOverrideClose(){
-  Serial.println("Manual close");
-
-  //sunet de avertizare
   digitalWrite(activeBuzzer, HIGH);
   delay(1);
   digitalWrite(activeBuzzer, LOW);
   delay(1000);
 
-  //servo-ul inchide capacul
   servoMotor.write(20);
 
-  //sunet de avertizare
+  delay(250);
   digitalWrite(activeBuzzer, HIGH);
   delay(1);
   digitalWrite(activeBuzzer, LOW);
   delay(1);
 
-  Serial.println("Manual close ended");
+  open = false;
 }
 
 void loop(){
@@ -125,6 +114,9 @@ void loop(){
     irrecv.resume();
   }
 
-  if(distance <= 4) autonomousMode(); //daca mana trece la mai putin de 4cm, se porneste modul autonom
-
+  if(distance <= 3 && enableAuto) autonomousMode();
+  if(distance <= 3 && !enableAuto){
+    if(open) manualOverrideClose();
+    else manualOverrideOpen();
+  }
 }
